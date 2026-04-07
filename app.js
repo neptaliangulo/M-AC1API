7499b65998c95d9b564d997f74b76ca48b76bf11
neptaliangulo <neptali.angulo@estudiant.fjaverianas.com>
2026-04-07 18:25:02 +0200

    add slugify to welcome message

--- app.js
+++ app.js
@@ -1,21 +1,19 @@
 const express = require('express')
 const products_routes = require('./routes/products.js')
+const users_routes = require('./routes/users.js')  
+const slugify = require('slugif')
 
-//Server instantiation
 const app = express()
 
-//Server configuration: template engine
 app.set('views', './views');
 app.set('view engine', 'pug');
 app.use(express.static('/views'));
 
-//Midleware
 app.use(express.json())
 app.use('/', products_routes)
+app.use('/api/users', users_routes)  
 
-//Server startup
 app.listen(5000, () => {
-    console.log('server is listening on port 5000')
+    const msg = slugify('server is listening on port 5000', { replacement: '*' })
+    console.log(msg)
 })
-
-
